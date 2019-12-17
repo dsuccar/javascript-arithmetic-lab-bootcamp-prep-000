0f2eee23aab460a65fcf8176cec60c5e20fa4aaf
Daniel Succar <danielsuccar@icloud.com>
2019-12-17 18:14:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-'a' = Math.floor(Math.random() * 1000)
-'b' = Math.floor(Math.random() * 1000)
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
 
 function add() {
   a += b
